5f020303f4d38197fd44876da6d6bf95d2efabc5
LUEJ9 <juan.luethy@coop.ch>
2024-06-03 11:47:15 +0200

    updated Webconfig as attempt to fix
    #2

--- backend/src/main/java/com/example/backend/WebConfig.java
+++ backend/src/main/java/com/example/backend/WebConfig.java
@@ -8,7 +8,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
-        // This forwards all requests to `index.html`
         registry.addViewController("/{spring:[a-zA-Z0-9-]+}").setViewName("forward:/index.html");
         registry.addViewController("/**/{spring:[a-zA-Z0-9-]+}").setViewName("forward:/index.html");
         registry.addViewController("/{spring:[a-zA-Z0-9-]+}/**{spring:[a-zA-Z0-9-]+}").setViewName("forward:/index.html");
