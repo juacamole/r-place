b71f1556b960927ca621a4e84cd36baa21eda119
LUEJ9 <juan.luethy@coop.ch>
2024-06-03 15:01:31 +0200

    changed 404 page and Spa Controller, possibly working
    #2

--- frontend/src/components/pages/ErrorPageNotFound.tsx
+++ frontend/src/components/pages/ErrorPageNotFound.tsx
@@ -1,35 +1,12 @@
-import CircleDesign from "../design-components/CircleDesign.tsx";
-import Logo from "../../assets/coop place logo.png";
-import {useEffect, useState} from "react";
+import {useEffect} from "react";
 import {useNavigate} from "react-router-dom";
 
 export default function ErrorPageNotFound() {
-
-    const navigate = useNavigate();
-    const [counter, setCounter] = useState<number>(5);
-
+    const navigate = useNavigate()
     useEffect(() => {
-        const interval = setInterval(() => {
-            setCounter((prevCounter) => {
-                if (prevCounter > 1) {
-                    return prevCounter - 1;
-                } else {
-                    navigate("/home");
-                    clearInterval(interval);
-                    return 0;
-                }
-            });
-        }, 1000);
-
-        return () => clearInterval(interval);
-    }, [navigate]);
-
+        console.log("Page not found")
+        navigate("/home");
+    }, []);
     return <div>
-        <img src={Logo} className={"logo"} alt={undefined}/>
-        <div className={"main-panel"}>
-            <h1>Page Not Found</h1>
-            <h2 className={"return-paragraph"}>You will be Redirected to our main page in {counter} seconds</h2>
-        </div>
-        <CircleDesign/>
     </div>
 }
