9e282f992b1b2a32f2a1ea502674106d41638cf9
LUEJ9 <juan.luethy@coop.ch>
2024-06-03 15:48:26 +0200

    made ws url wss

--- frontend/src/WSService.tsx
+++ frontend/src/WSService.tsx
@@ -8,7 +8,7 @@ export type WSServiceType = {
 
 export function WSService(): WSServiceType {
 
-    const ws = new WebSocket(`ws://r-place-xvtv.onrender.com/ws`);
+    const ws = new WebSocket(`wss://r-place-xvtv.onrender.com/ws`);
 
     const queue: string[] = [];
     const listenerQueue: ((message: MessageEvent) => void)[] = [];
