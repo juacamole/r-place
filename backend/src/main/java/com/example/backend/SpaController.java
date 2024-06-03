799ac3107bd48146f3a9e560634f85289d74a6ba
LUEJ9 <juan.luethy@coop.ch>
2024-06-03 14:41:34 +0200

    Testing with Spa Controller
    #2

--- backend/src/main/java/com/example/backend/SpaController.java
+++ backend/src/main/java/com/example/backend/SpaController.java
@@ -3,14 +3,15 @@ package com.example.backend;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
 
 @RestController
 @RequiredArgsConstructor
 public class SpaController {
 
     @GetMapping("/home")
-    public String bruh() {
-        return "this work";
+    public RedirectView redirectToHtml() {
+        return new RedirectView("/index.html");
     }
 
 }
