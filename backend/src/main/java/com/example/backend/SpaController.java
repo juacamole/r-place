6ebd5fcbbb52f659d9a661c87ec319a0af46a4cd
LUEJ9 <juan.luethy@coop.ch>
2024-06-24 10:39:54 +0200

    More Roots to SpaController

--- backend/src/main/java/com/example/backend/SpaController.java
+++ backend/src/main/java/com/example/backend/SpaController.java
@@ -9,9 +9,30 @@ import org.springframework.web.servlet.view.RedirectView;
 @RequiredArgsConstructor
 public class SpaController {
 
-    @GetMapping
-    public RedirectView redirectToHtml() {
+    @GetMapping("/home")
+    public RedirectView redirectFromHome() {
         return new RedirectView("/index.html");
     }
+
+    @GetMapping("/")
+    public RedirectView redirectFromRoot() {
+        return new RedirectView("/index.html");
+    }
+
+    @GetMapping("/register")
+    public RedirectView redirectFromRegister() {
+        return new RedirectView("/index.html");
+    }
+
+    @GetMapping("/register/2")
+    public RedirectView redirectFromRegister2() {
+        return new RedirectView("/index.html");
+    }
+
+    @GetMapping("/settings")
+    public RedirectView redirectFromSettings() {
+        return new RedirectView("/index.html");
+    }
+
 
 }
